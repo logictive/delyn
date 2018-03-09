809d79bd9f522596bfa4aa629d6d9249e5940afc
David Lewis <david@logictive.co.uk>
2018-03-09 20:18:31 +0000

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,105 @@
+import gulp from "gulp";
+import {spawn} from "child_process";
+import hugoBin from "hugo-bin";
+import gutil from "gulp-util";
+import flatten from "gulp-flatten";
+import postcss from "gulp-postcss";
+import autoprefixer from "autoprefixer";
+import sourcemaps from "gulp-sourcemaps";
+import sass from "gulp-sass";
+import cssNano from "gulp-cssnano";
+import presetEnv from "postcss-preset-env";
+import BrowserSync from "browser-sync";
+import webpack from "webpack";
+import webpackConfig from "./webpack.conf";
+
+const browserSync = BrowserSync.create();
+
+// Hugo arguments
+const hugoArgsDefault = ["-d", "../dist", "-s", "site", "-v"];
+const hugoArgsPreview = ["--buildDrafts", "--buildFuture"];
+
+// Development tasks
+gulp.task("hugo", (cb) => buildSite(cb));
+gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
+
+// Build/production tasks
+gulp.task("build", ["css", "js", "fonts"], (cb) => buildSite(cb, [], "production"));
+gulp.task("build-preview", ["css", "js", "fonts"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
+
+// Compile CSS with PostCSS
+gulp.task("css", () => {
+  const processors = [
+    presetEnv(),
+    autoprefixer()
+  ];
+
+
+  gulp.src("./src/scss/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: "nested", // libsass doesn't support expanded yet
+      precision: 10,
+      includePaths: ["."]
+    }))
+    .pipe(postcss(processors))
+    .pipe(cssNano())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
+});
+
+// Compile Javascript
+gulp.task("js", (cb) => {
+  const myConfig = Object.assign({}, webpackConfig);
+
+  webpack(myConfig, (err, stats) => {
+    if (err) throw new gutil.PluginError("webpack", err);
+    gutil.log("[webpack]", stats.toString({
+      colors: true,
+      progress: true
+    }));
+    browserSync.reload();
+    cb();
+  });
+});
+
+// Move all fonts in a flattened directory
+gulp.task("fonts", () => (
+  gulp.src("./src/fonts/**/*")
+    .pipe(flatten())
+    .pipe(gulp.dest("./dist/fonts"))
+    .pipe(browserSync.stream())
+));
+
+// Development server with browsersync
+gulp.task("server", ["hugo", "css", "js", "fonts"], () => {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/css/**/*.css", ["css"]);
+  gulp.watch("./src/fonts/**/*", ["fonts"]);
+  gulp.watch("./site/**/*", ["hugo"]);
+});
+
+/**
+ * Run hugo and build the site
+ */
+function buildSite(cb, options, environment = "development") {
+  const args = options ? hugoArgsDefault.concat(options) : hugoArgsDefault;
+
+  process.env.NODE_ENV = environment;
+
+  return spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
+    if (code === 0) {
+      browserSync.reload();
+      cb();
+    } else {
+      browserSync.notify("Hugo build failed :(");
+      cb("Hugo build failed");
+    }
+  });
+}
---
+++ site/content/pages/about.md
@@ -0,0 +1,9 @@
+---
+title: "About"
+description: "This is a test"
+date: 2018-03-09T17:01:46Z
+draft: false
+menu: "main"
+---
+
+This is a test
